bab40be755bd8ef1e35be9eaa543371d19ea9ee3
甘鸿谨 <1925286941@qq.com>
2023-08-08 09:56:21 +0800

    离线客户端

---
+++ mbutton.h
@@ -0,0 +1,26 @@
+#ifndef MBUTTON_H
+#define MBUTTON_H
+
+#include <QWidget>
+#include <QEnterEvent>
+
+namespace Ui {
+class mButton;
+}
+
+class mButton : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit mButton(QWidget *parent = nullptr);
+    ~mButton();
+
+    void seticon(QChar mchar, QString text);
+private:
+    Ui::mButton *ui;
+    void enterEvent(QEnterEvent *event);
+    void leaveEvent(QEvent *event);
+};
+
+#endif // MBUTTON_H
---
+++ rankingbox.h
@@ -0,0 +1,28 @@
+#ifndef RANKINGBOX_H
+#define RANKINGBOX_H
+
+#include <QWidget>
+#include <QPropertyAnimation>
+#include <QEnterEvent>
+
+namespace Ui {
+class RankingBox;
+}
+
+class RankingBox : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit RankingBox(QWidget *parent = nullptr);
+    ~RankingBox();
+
+private:
+    Ui::RankingBox *ui;
+    bool eventFilter(QObject *watched, QEvent *event);
+    QPropertyAnimation * animetion;
+    void enterEvent(QEnterEvent *event);
+    void leaveEvent(QEnterEvent *event);
+};
+
+#endif // RANKINGBOX_H
